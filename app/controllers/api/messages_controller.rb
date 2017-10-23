fe0f1a5d91f70f6afa485f8ec56cb53daa42587b
github username <github email address>
2017-10-23 10:35:55 -0400

    home page

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,19 +1,15 @@
 class Api::MessagesController < ApplicationController
-    def self.send_message(message)
-      msg = send_message(message)
-      debugger
-    end
 
-    def initialize
-      # To find TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN visit
-      # https://www.twilio.com/user/account
-      account_sid = 'AC14aa1a7fce2fd4f7ef91d392b45f503f'
-      auth_token  = '24c0445b38b2035e7e8df4911dcbc338'
-      @client = Twilio::REST::Client.new(account_sid, auth_token)
-      message = @client.messages.create(
-      :body => 'All in the game, yo',
-      :to   => '+14108675309',
-      :from => '+15005550006')
-      debugger
-    end
+  def index
+  end
+
+  def create
+    debugger
+    @chatroom = Chatroom.where("name=?",params[:name])
+    @messages = @chatroom[0].messages
+    render json: @messages
+  end
+
+  private
+
 end
---
+++ app/controllers/api/texts_controller.rb
@@ -0,0 +1,20 @@
+class Api::TextsController < ApplicationController
+  def self.send_message(message)
+    msg = send_message(message)
+    debugger
+  end
+
+      def initialize
+        # To find TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN visit
+        # https://www.twilio.com/user/account
+        account_sid = 'AC14aa1a7fce2fd4f7ef91d392b45f503f'
+        auth_token  = '24c0445b38b2035e7e8df4911dcbc338'
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+        message = @client.messages.create(
+        :body => 'All in the game, yo',
+        :to   => '+14108675309',
+        :from => '+15005550006')
+        debugger
+      end
+
+end
